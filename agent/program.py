c747c94ad6d8633983698e6f39a1b4d761f67d94
abogoslovska <anabelkana@gmail.com>
2023-05-04 11:16:33 +1000

    BREADTH = 13 DEPTH = 2

--- agent/program.py
+++ agent/program.py
@@ -16,7 +16,7 @@ import math
 # intended to serve as an example of how to use the referee API -- obviously
 # this is not a valid strategy for actually playing the game!
 
-BREADTH = 16
+BREADTH = 13
 DEPTH = 2
 
 DIM = 7
