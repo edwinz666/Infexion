2b2014e145a8edeb9d92acf1fdda3d6dd832c95e
Edwin <e.zhu2@student.unimelb.edu.au>
2023-05-13 23:39:01 +1000

    add some comments to code

--- agent/coverage.py
+++ agent/coverage.py
@@ -1,11 +1,13 @@
-from calendar import c
-
-
 DIM = 7
 DIRECTIONS = ((1,-1), (1,0), (0,1), (-1,1), (-1,0), (0,-1))
 POWER_DISTANCE_COVERAGE = {1: (1,0,0), 2: (2,2,0), 3: (2,3,2), 4: (2,3,3), 5: (2,3,3), 6: (2,3,3)}
 
+
 def generateCoverageDict():
+    """
+    Initializes coverage of each position on the board for a particular player
+    """
+
     coverage = {}
 
     for i in range(DIM):
@@ -13,63 +15,26 @@ def generateCoverageDict():
             coverage[(i,j)] = 0
 
     return coverage
-#def generateCoveragePositionPower():
-#    coveragePositionPower = {}
-
-#    for i in range(DIM):
-#        for j in range(DIM):
-#            position = (i, j)
-#            for power in range(1,4):
-#                #toAddBasedOnDistanceFromNode = POWER_DISTANCE_COVERAGE[power]
-#                # list of positions covered for each (position, power) pair AND how many times its covered?
-#                covered = []
-#                for direction in DIRECTIONS:
-#                    tempPower = power # tempPower = 0 .... while tempPower < 3 ... tempPower += 1
-#                    tempR = position[0]
-#                    tempQ = position[1]
-
-#                    while tempPower:
-#                        newR = tempR + direction[0]
-#                        newQ = tempQ + direction[1]
-
-#                        # require both r and q to be positive, and also less than the dimension
-#                        if newR < 0:
-#                            newR = DIM - 1
-#                        elif newR >= DIM:
-#                            newR = 0
-
-#                        if newQ < 0:
-#                            newQ = DIM - 1
-#                        elif newQ >= DIM:
-#                            newQ = 0 
-#                        if (newR, newQ) not in covered:
-#                            covered.append((newR, newQ))
-                    
-#                        tempR = newR
-#                        tempQ = newQ
-
-#                        tempPower -= 1
-                
-#                coveragePositionPower[(position, power)] = covered
-#                if power == 3:
-#                    for restPower in range(4, 7):
-#                        coveragePositionPower[(position, restPower)] = covered
-#    return coveragePositionPower
 
 
 def generateCoveragePositionPower():
+    """
+    Generates a dictionary where the keys are each (position, power) pairs for every 
+    position on the 7x7 board, and for every power from 1 to 6.
+
+    The output is what nodes are covered, and how many times it is covered.
+    """
     coveragePositionPower = {}
 
     for i in range(DIM):
         for j in range(DIM):
             position = (i, j)
             for power in range(1,DIM):
-                positionCoverage = {} #generateCoverageDict()
+                positionCoverage = {} 
                 toAddBasedOnDistanceFromNode = POWER_DISTANCE_COVERAGE[power]
-                # list of positions covered for each (position, power) pair AND how many times its covered?
 
                 for direction in DIRECTIONS:
-                    tempPower = 0 # tempPower = 0 .... while tempPower < 3 ... tempPower += 1
+                    tempPower = 0 
                     tempR = position[0]
                     tempQ = position[1]
 
@@ -104,26 +69,14 @@ def generateCoveragePositionPower():
 COVERAGE_POSITION_POWER = generateCoveragePositionPower()
 
 
-
-#def getCoverages(board):
-#    redCoverage = generateCoverageDict()
-#    blueCoverage = generateCoverageDict()
-
-#    # 2.
-#    for (position, (colour, power)) in board.items():
-#        for covered in COVERAGE_POSITION_POWER[(position, power)]:
-#            if colour == 'r':
-#                redCoverage[covered] += 1
-#            else:
-#                blueCoverage[covered] += 1
-
-#    return (redCoverage, blueCoverage)
-
 def getCoverages(board):
+    """
+    Returns coverage for each colour for each position on the 7x7 board
+    """
+
     redCoverage = generateCoverageDict()
     blueCoverage = generateCoverageDict()
 
-    # 2.
     for (position, (colour, power)) in board.items():
         for (coveredPosition, coveredTimes) in COVERAGE_POSITION_POWER[(position, power)].items():
             if colour == 'r':
@@ -133,31 +86,17 @@ def getCoverages(board):
 
     return (redCoverage, blueCoverage)
 
-#def peaceful(board):
-#    redCoverage = {}
-#    blueCoverage = {}
 
-#    # 2.
-#    for (position, (colour, power)) in board.items():
-#        for covered in COVERAGE_POSITION_POWER[(position, power)]:
-#            if colour == 'r':
-#                redCoverage[covered] = 1
-#            else:
-#                blueCoverage[covered] = 1
-    
-#    # 3.
-#    for (position, (colour, power)) in board.items():
-#        if (position in redCoverage.keys() and colour == 'b' or 
-#            position in blueCoverage.keys() and colour == 'r'):
-#            return False
-    
-#    return True
 
 def peaceful(board):
+    """
+    Checks if any captures can be made in a position
+    """
+
     redCoverage = {}
     blueCoverage = {}
 
-    # 2.
+    # get coverages for each colour
     for (position, (colour, power)) in board.items():
         for (coveredPosition, coveredTimes) in COVERAGE_POSITION_POWER[(position, power)].items():
             if colour == 'r':
@@ -171,29 +110,39 @@ def peaceful(board):
                 else:
                     blueCoverage[coveredPosition] = coveredTimes
     
-    # 3.
+    # if a piece is covered by the opponent, a capture is available
     for (position, (colour, _)) in board.items():
         if (position in redCoverage.keys() and colour == 'b' or 
             position in blueCoverage.keys() and colour == 'r'):
             return False
     
+    # no captures are available
     return True
 
 
-# gives each node's power (or something else?) to a certain player based on
-# the node's colour and 
+
 def evaluateAtkDef(board: dict[tuple, tuple], colourToMove):
+    """
+    Evaluation function of a position, based on coverage of nodes for each colour.
+
+    Primary evaluation assigns the power of each piece on the board to a colour, 
+    and adjusts for whose move it is.
+
+    Secondary evaluation is used in the case of a tie in the primary evaluation,
+    favouring the side who covers any node on the board more times overall.
+  
     # 1. generate two arrays 7x7 ? for both colours
-    # 2. go over every node, and add to covered squares in array based on colour
+    # 2. go over every node, and add to positions covered and how many times based on colour
     # 3. go over every node again, but compare array coverages based on colour
-    # 4.    for every node ... add power based on who wins
-    # 5. if colourToMove is RED ... remove blue's best capture if available
+            and add power based on who wins
+    # 4. evaluate by the overall power of pieces covered first, but then after by overall coverage numbers
+    """
 
     # 1. 
     colourToMoveCoverage = generateCoverageDict()
     colourJustPlayedCoverage = generateCoverageDict()
 
-    # MAYBE ONLY NEED LIST FOR colourJustPlayed ... to remove the best capture for that colour
+
     colourToMoveScore = 0
     maxJustPlayedPowerCoverage = 0
     secondaryOverlappingScore = 0
@@ -213,17 +162,20 @@ def evaluateAtkDef(board: dict[tuple, tuple], colourToMove):
         if defendingColour == colourToMove:           
             if colourToMoveCoverage[position] >= colourJustPlayedCoverage[position]:
                 colourToMoveScore += power
+            # defending colour is the colour to move and a favourable capture can be made by the colour that just played
             else:
                 colourToMoveScore -= power
                 if power > maxJustPlayedPowerCoverage:
                     maxJustPlayedPowerCoverage = power
-        else: # defendingColour is the colour who just played
+
+        # defendingColour is the colour who just played
+        else: 
             if colourJustPlayedCoverage[position] >= colourToMoveCoverage[position]:
                 colourToMoveScore -= power
             else:
                 colourToMoveScore += power
 
-    ### add component that sorts by this first, but then after by overlapping territory?
+    # 4. 
     if colourToMove == 'r':
         return (colourToMoveScore + maxJustPlayedPowerCoverage, secondaryOverlappingScore)
     else:
